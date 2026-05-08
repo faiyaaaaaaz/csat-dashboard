f2e79c3aba191821c69bd99309ef42d26278bf1e
faiyaaaaaaz <faiyaz@nextventures.io>
2026-05-08 16:06:19 +0600

    Add 'notranslate' attribute to HTML and body
    Added 'notranslate' class and attribute to HTML and body elements.

--- app/layout.js
+++ app/layout.js
@@ -4,12 +4,15 @@ import AppShellClient from "./components/AppShellClient";
 export const metadata = {
   title: "AI Auditor & Insights Platform",
   description: "Internal audit system",
+  other: {
+    google: "notranslate",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="notranslate" translate="no">
+      <body className="notranslate" translate="no">
         <AppShellClient>{children}</AppShellClient>
       </body>
     </html>
